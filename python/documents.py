2b46f802495bf471f6abe8b81fde3a12b9d52128
j-max <jmax.barry@gmail.com>
2023-03-18 17:33:19 -0500

    Add cumulative_word_count function

--- python/documents.py
+++ python/documents.py
@@ -6,6 +6,135 @@ from nltk.tokenize import sent_tokenize
 
 from summary_statistics import count_words
 
+
+class Sentence:
+
+    """
+    Take a sentence string and describe it:
+        Length
+        Number of words
+        Type of sentence based on punctuation
+
+    """
+
+    def __init__(self, sentence_string):
+        
+        self.sentence_string = sentence_string
+        self.word_tokens = []
+        self.word_tokenize()
+        self.punctuation_marks = []
+        self.find_punctuation_marks()
+        self.type_of_sentence = ""
+        self.determine_type_of_sentence()
+        self.string_length = len(self.sentence_string)
+        self.word_count = len(self.word_tokens)
+        # TODO Add timestamps to journal sentences
+        
+    
+    def word_tokenize(self):
+        
+        tokenizer = RegexpTokenizer(r"\w+")
+        self.word_tokens = tokenizer.tokenize(self.sentence_string)
+
+    
+    def find_punctuation_marks(self):
+
+        # find elipses first, so that the match doesn"t stop after a period
+        tokenizer = RegexpTokenizer(r"\.{3}|[!,;:?.]")
+        self.punctuation_marks = tokenizer.tokenize(self.sentence_string)
+        
+    
+    def determine_type_of_sentence(self):
+
+        sentence_types = {
+            ".": "declarative",
+            "?": "question",
+            "!": "exclamation"}
+        
+        # determine the sentence type by the last punctuation token
+        if self.punctuation_marks[-1] in sentence_types:
+            self.type_of_sentence = sentence_types[self.punctuation_marks[-1]]
+        else:
+            self.sentence_type = "undefined"
+    
+
+
+class Document:
+
+    def __init__(self, path_to_document):
+
+        self.path_to_document = path_to_document
+        self.document_name = ""
+        self.document_string = ""
+        self.read_in_document()
+        self.sentences = []
+        self.create_sentences()
+
+    
+    def read_in_document(self):
+        
+        """
+        Read in a document given a path to the document
+        """
+        
+        # create Path object to allow access to document name
+        doc_path = Path(self.path_to_document)
+
+        with open(Path(doc_path), "r") as read_file:
+            self.document_string = read_file.read().replace("\n", " ")
+            self.document_name = doc_path.name
+                   
+
+    def create_sentences(self):
+
+        # TODO Figure out how to parse the date stamps of a journal entry
+        tokenized_sentences = sent_tokenize(self.document_string)
+        self.sentences = [Sentence(sentence_string) for sentence_string in tokenized_sentences]
+
+    def cumulative_word_count(self):
+        """Take a list of sentences, and count new words cumulatively.
+ 
+        The cumulative_word_count() function returns a list which 
+        counts the number of new words added as the document grows.  
+
+        The larger the document, the smaller the slope will be, since 
+        it is a higher probabilty that each successive word added will
+        have been already used in the document.
+
+        The changes in the slope of the graph may give a sense of
+        where new ideas are introduced.           
+
+        :param sentence_list: _description_
+        :type sentence_list: list
+        :return: a list of the cumulative count
+        :rtype: list
+        """
+
+        # Put all sentence word tokens into one list
+        ordered_sentence_tokens = []
+        for sentence in self.sentences:
+            ordered_sentence_tokens.extend(sentence.word_tokens)
+        
+        used_words_list = []
+        cumulative_count = 0
+        cumulative_count_array = []
+        for token in ordered_sentence_tokens:
+            if token not in used_words_list:
+                used_words_list.append(token)
+                cumulative_count+=1
+            cumulative_count_array.append(cumulative_count)
+        
+        return cumulative_count_array
+
+
+
+
+
+    
+
+
+
+
 class Corpus:
 
     def __init__(self, path_to_documents_folder):
@@ -72,90 +201,5 @@ class Corpus:
             self.document_tokens = tokenized_words
             return tokenized_words
 
-
-
-class Document:
-
-    def __init__(self, path_to_document):
-
-        self.path_to_document = path_to_document
-        self.document_name = ""
-        self.document_string = ""
-        self.read_in_document()
-        self.sentences = []
-        self.create_sentences()
-
-    
-    def read_in_document(self):
-        
-        """
-        Read in a document given a path to the document
-        """
-        
-        # create Path object to allow access to document name
-        doc_path = Path(self.path_to_document)
-
-        with open(Path(doc_path), "r") as read_file:
-            self.document_string = read_file.read().replace("\n", " ")
-            self.document_name = doc_path.name
-                   
-
-    def create_sentences(self):
-
-        # TODO Figure out how to parse the date stamps of a journal entry
-        tokenized_sentences = sent_tokenize(self.document_string)
-        self.sentences = [Sentence(sentence_string) for sentence_string in tokenized_sentences]
-
-
-class Sentence:
-
-    """
-    Take a sentence string and describe it:
-        Length
-        Number of words
-        Type of sentence based on punctuation
-
-    """
-
-    def __init__(self, sentence_string):
-        
-        self.sentence_string = sentence_string
-        self.word_tokens = []
-        self.word_tokenize()
-        self.punctuation_marks = []
-        self.find_punctuation_marks()
-        self.type_of_sentence = ""
-        self.determine_type_of_sentence()
-        self.string_length = len(self.sentence_string)
-        self.word_count = len(self.word_tokens)
-        
-    
-    def word_tokenize(self):
-        
-        tokenizer = RegexpTokenizer(r"\w+")
-        self.word_tokens = tokenizer.tokenize(self.sentence_string)
-
-    
-    def find_punctuation_marks(self):
-
-        
-        # find elipses first, so that the match doesn"t stop after a period
-        tokenizer = RegexpTokenizer(r"\.{3}|[!,;:?.]")
-        self.punctuation_marks = tokenizer.tokenize(self.sentence_string)
-        
-    
-    def determine_type_of_sentence(self):
-
-
-        sentence_types = {
-            ".": "declarative",
-            "?": "question",
-            "!": "exclamation"}
-        
-        # determine the sentence type by the last punctuation token
-        if self.punctuation_marks[-1] in sentence_types:
-            self.type_of_sentence = sentence_types[self.punctuation_marks[-1]]
-        else:
-            self.sentence_type = "undefined"
 
 
